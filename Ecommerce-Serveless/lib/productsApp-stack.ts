e240817ab83819fb1f970adba731267971bfe792
Aldry Albuquerque <albuquerquealdry>
2023-07-29 07:48:34 -0400

    Adicionar modulo layer

--- Ecommerce-Serveless/lib/productsApp-stack.ts
+++ Ecommerce-Serveless/lib/productsApp-stack.ts
@@ -2,13 +2,32 @@ import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
 import { Construct } from "constructs"
+import * as dynadb from "aws-cdk-lib/aws-dynamodb"
+import * as ssm from "aws-cdk-lib/aws-ssm"
 
 export class ProductsAppStack extends cdk.Stack {
     readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+    readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
+    readonly productsDdb: dynadb.Table
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps){
         super(scope, id, props)
 
+        this.productsDdb = new  dynadb.Table(this, "ProductsDdb", {
+            tableName: "products",
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            partitionKey: {
+                name: "id",
+                type: dynadb.AttributeType.STRING
+            },
+            billingMode: dynadb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        })
+        //Products Layer
+        const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
+        const productstLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn",productsLayerArn )
+        
         this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "Products FetchFunction", {
             runtime: lambda.Runtime.NODEJS_16_X,
             functionName: "ProductsFetchFunction",
@@ -20,6 +39,30 @@ export class ProductsAppStack extends cdk.Stack {
                 minify: true,
                 sourceMap: false
             },
+            environment: {
+                PRODUCTS_DDB: this.productsDdb.tableName
+            },
+            layers: [productstLayer]
         })
+
+        this.productsDdb.grantReadData(this.productsFetchHandler)
+
+        this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this, "Products AdminFuction", {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            functionName: "ProductsAdminFuction",
+            entry: "lambda/products/productsAdminFuction.ts",
+            handler: "handler",
+            memorySize: 128,
+            timeout: cdk.Duration.seconds(5),
+            bundling:{
+                minify: true,
+                sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DDB: this.productsDdb.tableName
+            },
+            layers: [productstLayer]
+        })
+        this.productsDdb.grantWriteData(this.productsAdminHandler)
     }
 }
